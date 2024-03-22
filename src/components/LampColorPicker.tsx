e15529dbe6341c28ede287902318e64d61736088
rudolfk1 <kurzanovrudolf@gmail.com>
2024-03-21 22:34:57 -0400

    fixed toggles

--- src/components/LampColorPicker.tsx
+++ src/components/LampColorPicker.tsx
@@ -13,7 +13,7 @@ function LampColorPicker (){
         <button
           key={color}
           style={{ backgroundColor: color }}
-          className="w-10 h-10 rounded-full border-[2.5px] shadow-xl border-zinc-200/50 hover:border-stone-400 focus:border-gray-800 focus:border-4"
+          className="w-10 h-10 rounded-full border-[2.5px] shadow-xl border-zinc-200/50 hover:border-stone-400 focus:border-zinc-700 focus:border-4"
           onClick={() => {setPreselectedColor(color), changeColor(color)}}
         />
       ))}
--- src/components/LightToggle.tsx
+++ src/components/LightToggle.tsx
@@ -1,6 +1,4 @@
-
 import { MdOutlinePowerOff, MdOutlinePower } from "react-icons/md";
-
 
 interface LightToggleProps {
   isLightOn: boolean;
@@ -8,18 +6,20 @@ interface LightToggleProps {
 }
 
 function LightToggle({ isLightOn, toggleFeatures }: LightToggleProps) {
-  // Determine icon color based on isLightOn
-  const iconColor = isLightOn ? "black" : "white";
+  // Combine common classes for icons with dynamic color class
+  const iconClasses = `w-6 h-6 ${
+    isLightOn ? 'text-zinc-800' : 'text-zinc-300'
+  }`;
 
   return (
     <button
         onClick={toggleFeatures}
-        className="w-10 h-10 rounded-full border-[2.5px] shadow-xl border-zinc-200/40 hover:border-stone-500 focus:border-4 focus:border-gray-400 focus:outline-none flex justify-center items-center"
+        className="w-10 h-10 rounded-full border-[2.5px] shadow-xl border-zinc-200/40 hover:border-stone-500 focus:border-4 focus:border-zinc-700 focus:outline-none flex justify-center items-center"
     >
         {isLightOn ? (
-        <MdOutlinePower size={24} style={{ color: iconColor }} />
+        <MdOutlinePower className={iconClasses} />
         ) : (
-        <MdOutlinePowerOff size={24} style={{ color: iconColor }} />
+        <MdOutlinePowerOff className={iconClasses} />
         )}
     </button>
   );
